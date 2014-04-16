595a98a4106e15596d6a9da5745780c89b4d365e
Ben Calegari <bencalegari@gmail.com>
2014-04-15 23:17:24 -0400

    Remove old JS and add clock to page

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,3 @@
 $(document).ready(function() {
-  $('form').on('submit', function(e) {
-//    e.preventDefault();
-//    var wfaResponse = callWFA($('#drinks').val(), $('#hours').val(), $('#sex').val(), $('#weight').val());
-  });
-
-
-  var callWFA = function(drinks, hours, sex, weight) {
-    var url = 'http://api.wolframalpha.com/v2/query?appid=***REMOVED***';
-    var query = 'BAC ' + drinks + ' drinks ' + hours + ' hours ' + sex.toLowerCase() + ' ' + weight + 'lb';
-    console.log(query);
-    $.get(url, { 'input': query, 'format': 'plaintext'},
-
-        function(data) {
-          debugger;
-          console.log(data);
-        }
-    );
-  }
-
+  var clock = $('#countdown').FlipClock({});
 });
